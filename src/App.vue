8080d5fd4715e7bdc00cc4ff9456b999154f216e
AbubakerSallam <abubaker4sallam@gmail.com>
2025-07-17 19:25:19 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import TodoHeader from './components/TodoHeader.vue';
+</script>
+
+<template>
+ <TodoHeader/> 
+  <RouterView />
+</template>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Playpen+Sans+Arabic:wght@100..800&display=swap');
+
+*{
+  font-family: 'Playpen Sans Arabic', Courier, monospace;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.container{
+  max-width: 1100px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/TodoCreator.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { reactive, ref } from 'vue';
+
+const todo = ref("Testing");
+
+const todoState = reactive({
+    todo: "Testing",
+});
+</script>
+<template>
+    <div class="input-wrap">
+        <input type="text" v-model="todoState.todo">
+        <button>انشاء</button>
+    </div>
+    <p>{{ todoState.todo }}</p>
+</template>
+
+
+
+<style lang="scss" scoped>
+.input-wrap {
+    display: flex;
+    transition: 250ms ease;
+    border: 2px solid #41b880;
+
+    &:focus-within {
+        box-shadow: 0 -4px 6px -1px rgp(0 0 0 / 0.1),
+            0 -2px 4px rgp(0 0 0 / 0.1);
+    }
+
+    input {
+        width: 100%;
+        padding: 8px 6px;
+        border: none;
+
+        &:focus {
+            outline: none;
+        }
+    }
+
+    button {
+        padding: 8px 16px;
+        border: none;
+        font-family: 'Playpen Sans Arabic',
+
+    }
+}
+</style>
